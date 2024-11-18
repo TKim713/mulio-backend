094ed3c7aaa37295ff444ca5a8397921c2748b64
TKim713 <thienkimbui10@gmail.com>
2024-11-18 14:14:15 +0700

    Fix CartResponse

--- src/main/kotlin/com/api/mulio_backend/helper/response/CartResponse.kt
+++ src/main/kotlin/com/api/mulio_backend/helper/response/CartResponse.kt
@@ -1,6 +1,7 @@
 package com.api.mulio_backend.helper.response
 
 data class CartResponse(
+    var cartId: String? = null,
     var products: List<CartProductResponse> = emptyList(),
     var totalNumber: Int = 0,
     var totalPrice: Float = 0f,
